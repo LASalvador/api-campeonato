aa46e267fae8b44aab4a8db59df8cf16d0c1b7b3
Lucas Salvador <lucas.salvador11358@gmail.com>
2021-02-24 07:33:53 -0300

    adicionando post de competitor

--- api/src/routes.py
+++ api/src/routes.py
@@ -1,15 +1,30 @@
 from flask import request, json, jsonify
-from src import app, db
+from src import app, db, database
 from src.models import Competitor, Tournament
 
 @app.route('/')
 def index():
     return 'Server Works!!'
 
+@app.route('/competitor', methods=['POST'])
+def post_competitor():
+    data = request.json
+    name = data['name']
+
+    competitor = database.add_instance(Competitor, name=name)
+    
+    competitor_dict = competitor.asdict()
+
+    response = jsonify({
+        'competitor': competitor_dict
+    })
+
+    return response
+
 @app.route('/competitor')
 def get_competitor():
 
-    competitor_response = Competitor.query.all()
+    competitor_response = database.get_all(Competitor)
     competitor_list = []
 
     for competitor in competitor_response:
@@ -23,7 +38,7 @@ def get_competitor():
 
 @app.route('/tournament')
 def get_tournament():
-    tournament_response = Tournament.query.all()
+    tournament_response = database.get_all(Tournament)
     tournament_list = []
 
     for tournament in tournament_response:
