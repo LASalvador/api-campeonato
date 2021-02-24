d919acb47b92cfd9cca9b5e384168e714c0d0289
Lucas Salvador <lucas.salvador11358@gmail.com>
2021-02-23 22:15:26 -0300

    atualizando secret e atualizando database_uri para banco

--- api/src/config.py
+++ api/src/config.py
@@ -1,6 +1,6 @@
 class Config(object):
     DEBUG = True
     CSRF_ENABLED = True
-    SECRET = olamundo
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:olamundo@mysql-container:3306/api'
+    SECRET = 'olamundo'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:olamundo@mysql-container:3306/api'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
