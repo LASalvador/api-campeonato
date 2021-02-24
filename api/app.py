8db686ad84646a2404dcbaf9397a95ace4deaaf9
Lucas Salvador <lucas.salvador11358@gmail.com>
2021-02-23 22:15:56 -0300

    atualizando entrypoint da aplicação

--- api/app.py
+++ api/app.py
@@ -1,13 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return 'Hello world flask'
-
-@app.route("/api")
-def hello_api():
-    return 'API topzera com pythone e docker'
+from src import app
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
