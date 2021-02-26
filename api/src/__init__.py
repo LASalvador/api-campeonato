bc3e84fd6ce2889b2b5bff54b08c4800c8e0db6e
Lucas Salvador <lucas.salvador11358@gmail.com>
2021-02-26 00:39:25 -0300

    pegando config de variavel de ambiente

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from src.config import Config
+from src.config import app_config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+
+config_name = os.getenv('APP_ENV')
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_object(app_config[config_name])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
