1956c271a842805a692a9a86a7cd60b732924727
Lucas Salvador <lucas.salvador11358@gmail.com>
2021-02-26 00:39:05 -0300

    adicionando config de test

--- api/src/config.py
+++ api/src/config.py
@@ -4,3 +4,13 @@ class Config(object):
     SECRET = 'olamundo'
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:olamundo@mysql-container:3306/api'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ConfigTest(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:olamundo@mysql-container:3306/api_test'
+
+
+app_config = {
+    'development': Config,
+    'testing': ConfigTest
+}
