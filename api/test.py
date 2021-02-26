c646294078d825432f47a70ea6baa3a98d6b03cb
Lucas Salvador <lucas.salvador11358@gmail.com>
2021-02-26 07:51:54 -0300

    adicionando test para competitor

--- api/test.py
+++ api/test.py
@@ -34,6 +34,25 @@ class TestCompetitor(unittest.TestCase):
     def test_post_competitor(self):
         res = self.client.post('/competitor', data=self.competitor, content_type='application/json')
         self.assertEqual(res.status_code,200)
+        self.assertIn('Lucas', str(res.data))
+
+    def test_get_all_competitor(self):
+        res = self.client.post('/competitor', data=self.competitor, content_type='application/json')
+        self.assertEqual(res.status_code,200)
+
+        res = self.client.get('/competitors')
+        self.assertEqual(res.status_code,200)
+        self.assertIn('Lucas', str(res.data))
+
+    def test_get_one_competitor(self):
+        res = self.client.post('/competitor', data=self.competitor, content_type='application/json')
+        self.assertEqual(res.status_code,200)
+
+        id = res.data['id']
+
+        res = self.client.get(f'/competitor/{id}')
+        self.assertEqual(res.status_code,200)
+        self.assertIn(id, str(res.data))
 
     def tearDown(self):
         with self.app.app_context():
